81732f7c32c5d6a76a7d6529a580596f1ec3e514
Xavier <luis.ochoa02@epn.edu.ec>
2025-12-06 20:04:55 -0500

    Add functional tests for conversion API

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,4 +7,59 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
+  test('Convert a valid input (10L)', function(done) {
+    chai.request(server)
+      .get('/api/convert?input=10L')
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.approximately(res.body.returnNum, 2.64172, 0.1);
+        assert.equal(res.body.initUnit, 'L');
+        assert.equal(res.body.returnUnit, 'gal');
+        done();
+      });
+  });
+
+  test('Convert an invalid unit (32g)', function(done) {
+    chai.request(server)
+      .get('/api/convert?input=32g')
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'invalid unit');
+        done();
+      });
+  });
+
+  test('Convert an invalid number (3/7.2/4kg)', function(done) {
+    chai.request(server)
+      .get('/api/convert?input=3/7.2/4kg')
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'invalid number');
+        done();
+      });
+  });
+
+  test('Convert an invalid number AND unit (3/7.2/4kilomegagram)', function(done) {
+    chai.request(server)
+      .get('/api/convert?input=3/7.2/4kilomegagram')
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'invalid number and unit');
+        done();
+      });
+  });
+
+  test('Convert with no number (kg)', function(done) {
+    chai.request(server)
+      .get('/api/convert?input=kg')
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.equal(res.body.initNum, 1);
+        assert.equal(res.body.initUnit, 'kg');
+        assert.approximately(res.body.returnNum, 2.20462, 0.1);
+        assert.equal(res.body.returnUnit, 'lbs');
+        done();
+      });
+  });
+
 });
